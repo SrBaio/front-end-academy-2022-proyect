a62e21c9248a2f230f3bafa642eede652b132143
SrBaio <90214928+SrBaio@users.noreply.github.com>
2023-02-28 18:06:32 -0300

    Primer commit

---
+++ scrips/script.js
@@ -0,0 +1,13 @@
+
+let persona = fetch('https://randomuser.me/api/?inc=name,location,email,dob,cell')
+console.log(persona)
+    
+
+/**nombre
+ * edad
+ * ciudad
+ * direccion
+ * email
+ * telefono
+ */
+
